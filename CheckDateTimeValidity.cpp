1ad4e97e4af55ae45b314ab8da108ef03b23d5a6
Manikgog <m4xin4t4@gmail.com>
2025-07-27 20:44:55 +0500

    ParseCountryJson is refactored

--- CheckDateTimeValidity.cpp
+++ CheckDateTimeValidity.cpp
@@ -1,46 +1,61 @@
-std::optional<std::string> CheckDateTimeValidity(const DateTime& dt) {
-    if (dt.year < 1) {
-        return {"year is too small"s};
+namespace {
+    constexpr int kMinYear = 1;
+    constexpr int kMaxYear = 9999;
+    constexpr int kMonthsInYear = 12;
+    constexpr int kMaxHour = 23;
+    constexpr int kMaxMinute = 59;
+    constexpr int kMaxSecond = 59;
+
+    bool IsLeapYear(int year) {
+        return (year % 4 == 0) && !(year % 100 == 0 && year % 400 != 0);
     }
-    if (dt.year > 9999) {
-        {"year is too big"s};
+}
+
+std::optional<std::string> CheckDateTimeValidity(const DateTime& dt) {
+    if (dt.year < kMinYear) {
+        return {"year "s + std::to_string(dt.year) + " is too small"s};
+    }
+    if (dt.year > kMaxYear) {
+        return {"year "s + std::to_string(dt.year) + " is too big"s};
     }
 
     if (dt.month < 1) {
-        {"month is too small"s};
+        return {"month "s + std::to_string(dt.month) + " is too small"s};
     }
-    if (dt.month > 12) {
-        {"month is too big"s};
+    if (dt.month > kMonthsInYear) {
+        return {"month "s + std::to_string(dt.month) + " is too big"s};
     }
 
-    const bool is_leap_year = (dt.year % 4 == 0) && !(dt.year % 100 == 0 && dt.year % 400 != 0);
-    const array month_lengths = {31, 28 + is_leap_year, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    const bool is_leap_year = IsLeapYear(dt.year);
+    const std::array<int, kMonthsInYear> month_lengths = {31, 28 + is_leap_year, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
     if (dt.day < 1) {
-        {"day is too small"s};
+        return {"day "s + std::to_string(dt.day) + " is too small"s};
     }
     if (dt.day > month_lengths[dt.month - 1]) {
-        {"day is too big"s};
+        return {"day "s + std::to_string(dt.day) + " is too big"s};
     }
 
     if (dt.hour < 0) {
-        {"hour is too small"s};
+        return {"hour "s + std::to_string(dt.hour) + " is too small"s};
     }
-    if (dt.hour > 23) {
-        {"hour is too big"s};
+    if (dt.hour > kMaxHour) {
+        return {"hour "s + std::to_string(dt.hour) + " is too big"s};
     }
 
     if (dt.minute < 0) {
-        {"minute is too small"s};
+        return {"minute "s + std::to_string(dt.minute) + " is too small"s};
     }
-    if (dt.minute > 59) {
-        {"minute is too big"s};
+    if (dt.minute > kMaxMinute) {
+        return {"minute "s + std::to_string(dt.minute) + " is too big"s};
     }
 
     if (dt.second < 0) {
-        {"second is too small"};
+        return {"second "s + std::to_string(dt.second) + " is too small"s};
     }
-    if (dt.second > 59) {
-        {"second is too big"s};
+    if (dt.second > kMaxSecond) {
+        return {"second "s + std::to_string(dt.second) + " is too big"s};
     }
-} 
+
+    return std::nullopt;
+}
--- ParseCitySubjson.cpp
+++ ParseCitySubjson.cpp
@@ -1,12 +1,21 @@
+City CreateCityFromJson(const Json::Object& city_obj, const Country& country) {
+    return {
+        city_obj.at("name"s).AsString(),
+        city_obj.at("iso_code"s).AsString(),
+        country.phone_code + city_obj.at("phone_code"s).AsString(),
+        country.name,
+        country.iso_code,
+        country.time_zone,
+        country.languages
+    };
+}
+
 // Дана функция ParseCitySubjson, обрабатывающая JSON-объект со списком городов конкретной страны:
-void ParseCitySubjson(vector<City>& cities, const Json& json, const string& country_name,
-                      const string& country_iso_code, const string& country_phone_code, const string& country_time_zone,
-                      const vector<Language>& languages) {
-    for (const auto& city_json : json.AsList()) {
-        const auto& city_obj = city_json.AsObject();
-        cities.push_back({city_obj["name"s].AsString(), city_obj["iso_code"s].AsString(),
-                          country_phone_code + city_obj["phone_code"s].AsString(), country_name, country_iso_code,
-                          country_time_zone, languages});
+void ParseCitySubjson(vector<City>& cities, const Json& json, const Country& country) {
+    const auto& city_list = json.AsList();
+    cities.reserve(cities.size() + city_list.size());
+    for (const auto& city_json : city_list) {
+        cities.push_back(CreateCityFromJson(city_json.AsObject(), country));
     }
 }
 
@@ -24,7 +33,6 @@ void ParseCountryJson(vector<Country>& countries, vector<City>& cities, const Js
         for (const auto& lang_obj : country_obj["languages"s].AsList()) {
             country.languages.push_back(FromString<Language>(lang_obj.AsString()));
         }
-        ParseCitySubjson(cities, country_obj["cities"s], country.name, country.iso_code, country.phone_code,
-                         country.time_zone, country.languages);
+        ParseCitySubjson(cities, country_obj["cities"s], country);
     }
 }
